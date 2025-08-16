f4dcd25bfc931dfc1954bd2d40453e61e34dd7cc
Enexobirukti7777777 <biruktawitb499@gmail.com>
2025-08-17 01:16:16 +0300

    First commit

---
+++ lib/view/login_view.dart
@@ -0,0 +1,91 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:project_flutter1/firebase_options.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key , required this.title});
+final String title; 
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+// ✅ This is the state class (build method goes here)
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
+  void initState (){
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+    @override
+  void dispose(){
+      _email.dispose();
+      _password.dispose();
+      super.dispose();
+    }
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: const Text('Login'),
+      ),
+      body: FutureBuilder(
+        future:Firebase.initializeApp(
+ options: DefaultFirebaseOptions.currentPlatform,
+),
+        builder:(context, snapshot){
+          
+          return Column(
+        children:[ 
+        TextField(controller: _email,
+       obscureText: false,
+        enableSuggestions: false,
+        autocorrect: false,
+        keyboardType: TextInputType.emailAddress,
+        decoration:const InputDecoration(hintText: 'biruk123@gmail.com')),
+        TextField(controller: _password,
+        obscureText: true,
+        enableSuggestions: false,
+        autocorrect: false,
+        decoration: const InputDecoration(hintText: 'Enter password',),
+        
+        ),
+        TextButton(
+        onPressed: () async {
+          
+          final email = _email.text;
+          final password = _password.text;
+          try{
+            // ignore: unused_local_variable
+             FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: email,
+           password: password,
+           );
+          }
+          on FirebaseAuthException catch (e){
+             if(e.code == 'user-not-found'){
+              print('user not found');
+             }
+             
+          }
+
+
+           
+          
+        },
+      child: const Text('Login'),
+      ),
+  ]
+  );
+  }
+  ) 
+   );
+  }
+  
+  
+}
